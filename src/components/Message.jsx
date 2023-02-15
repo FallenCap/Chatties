881327ed784af0a2bb9c7847252e12ea39cda5e8
FallenCap <fallencap18@gmail.com>
2023-02-16 01:34:30 +0530

    4th commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,24 +3,33 @@ import { auth } from "../firbase";
 import "./css/Message.css";
 
 const styles = {
-  message: `d-flex position-relative text-center shadow-sm mt-5 py-2 px-3 rounded-pill w-25`,
-  send: `text-right bg-warning float-right flex-row-reverse plef`,
-  receive: `float-left`,
+  // message: `d-flex position-relative text-center shadow-sm mt-5 py-2 px-3 rounded-pill w-25`,
+  sendText: `send-msg text-end shadow-sm`,
+  receiveText: `receive-msg text-start ms-3`,
+  sendName: `fs-7 send-name`,
+  receiveName: `fs-7 receive-name ms-3`,
 };
 
 const Message = ({ message }) => {
-  const messageClass =
+  const messageText =
     message.uid === auth.currentUser.uid
-      ? `${styles.send}`
-      : `${styles.receive}`;
+      ? `${styles.sendText}`
+      : `${styles.receiveText}`;
+
+  const messageName =
+    message.uid === auth.currentUser.uid
+      ? `${styles.sendName}`
+      : `${styles.receiveName}`;
 
   return (
-    <div>
-      <div className={`${styles.message} ${messageClass}`}>
-        <p className="position-absolute mtop-1 name text-secondary">
-          {message.name}
-        </p>
-        <p className="chat">{message.text}</p>
+    <div className="flex-column mt-4 mb-4">
+      <div>
+        <div>
+          <label className={messageName}>{message.name}</label>
+        </div>
+        <div>
+          <p className={messageText}>{message.text}</p>
+        </div>
       </div>
     </div>
   );
